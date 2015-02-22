574c40bc7c2211d296cb50413f9cc863b4d0a4b3
thao Hoang <hoangthao811@gmail.com>
2015-02-22 12:06:46 -0800

    selectable dates
    to select the dates on the calendar

--- app/src/main/java/com/libraryspace/libraryspace/MainActivity.java
+++ app/src/main/java/com/libraryspace/libraryspace/MainActivity.java
@@ -2,26 +2,50 @@ package com.libraryspace.libraryspace;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.content.Intent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 
 
 public class MainActivity extends ActionBarActivity {
 
 
-    Button grpButton;
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button grpButton = (Button) findViewById(R.id.grpButton);
+        grpButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View ArgO) {
+                Intent intent = new Intent(getApplicationContext(), cal_group.class);
+                startActivity(intent);
+            }
+        });
+
+        Button indButton = (Button) findViewById(R.id.indButton);
+        indButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Indiv_cal.class);
+                startActivity(intent);
+
+            }
+        });
+
+        Button mediaButton = (Button) findViewById(R.id.medButton);
+        mediaButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MediaCal.class);
+                startActivity(intent);
+            }
+        });
     }
-
-    void openCalendar
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -29,6 +53,7 @@ public class MainActivity extends ActionBarActivity {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
--- app/src/main/java/com/libraryspace/libraryspace/cal_group.java
+++ app/src/main/java/com/libraryspace/libraryspace/cal_group.java
@@ -3,7 +3,18 @@ package com.libraryspace.libraryspace;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.widget.CalendarView;
+import android.widget.CalendarView.OnDateChangeListener;
+import android.widget.Toast;
 import android.view.MenuItem;
+import android.widget.ListView;
+import android.widget.ArrayAdapter;
+
+import android.widget.AdapterView.OnItemClickListener;
+
+
+
+
 
 
 public class cal_group extends ActionBarActivity {
@@ -12,8 +23,30 @@ public class cal_group extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.cal_group);
-    }
 
+        CalendarView calendar = (CalendarView) findViewById(R.id.calendarView1);
+        calendar.setOnDateChangeListener(new OnDateChangeListener() {
+
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year,
+                                            int month, int dayOfMonth) {
+                // TODO Auto-generated method stub
+                Toast.makeText(
+                        getApplicationContext(),
+                        "Selected date is :\n\n" + month + "-"
+                                + dayOfMonth + "-" + year, Toast.LENGTH_LONG).show();
+            }
+
+        });
+
+        setContentView(R.layout.cal_group);
+        String[] items = {"8:00 AM", "9:00 AM", "10:00 AM"};
+
+       ListView listView = (ListView) findViewById(R.id.listView);
+        listView.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items));
+
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
